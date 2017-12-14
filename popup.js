c32e26be555a5bdf155cc4f1a02a7440a097e6c7
Michael Li <michael.li1324@gmail.com>
2017-12-14 14:04:35 -0800

    style popup

--- popup.js
+++ popup.js
@@ -3,11 +3,11 @@ var toggleText = ['ON', 'OFF']
 
 document.addEventListener('DOMContentLoaded', function() {
   var toggleButton = $('#toggleButton');
-  console.log(toggleButton);
   toggleButton.click(function() {
     isTranslated = !isTranslated;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {translate: isTranslated});
+      console.log('sent')
     });
     $('#toggleButton').text(isTranslated ? toggleText[1] : toggleText[0]);
   });
